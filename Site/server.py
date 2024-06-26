dfdd6ed7b5393d4cf42660b3a1cce002c5d632cd
mleila1312 <leila.iksil@gmail.com>
2024-06-26 18:31:59 +0200

    new port

--- Site/server.py
+++ Site/server.py
@@ -103,4 +103,4 @@ def add_header_home(response):
 
 # lance le server
 if __name__ == "__main__":
-    app.run(debug = False, host='0.0.0.0', port=80)
+    app.run(debug = False, host='0.0.0.0', port=5000)
